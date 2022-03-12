5dd2bc46a28923ce1b686a50ad1a01ef82d8dc56
Maciej Tokarz <maciej.tokarz@teleste.com>
2022-03-12 08:09:18 +0100

    autonomous-car

---
+++ autonomous-car/autonomous-car.ino
@@ -0,0 +1,31 @@
+#include <MultiFunctionShield.h>
+
+int IR_01 = 2;
+
+void setup()
+{
+  pinMode(IR_01, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(IR_01) == LOW)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  // blinkLed(1000);
+}
+
+void blinkLed(int time)
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(time);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(time);
+}
