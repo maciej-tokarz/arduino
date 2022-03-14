fab0a9825ac66ef5bd9557f947798deca3741676
maciej-tokarz <maciej.tokarz@my-poi.pl>
2022-03-14 22:42:48 +0100

    motors

--- autonomous-car/autonomous-car.ino
+++ autonomous-car/autonomous-car.ino
@@ -1,25 +1,52 @@
 #include <MultiFunctionShield.h>
+#include <AFMotor.h>
 
-int IR_01 = 5;
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+// int IR_01 = 5;
 
 void setup()
 {
-  pinMode(IR_01, INPUT);
-  pinMode(LED_1_PIN, OUTPUT);
+  // pinMode(IR_01, INPUT);
+  // pinMode(LED_1_PIN, OUTPUT);
 }
 
 void loop()
 {
-  if (digitalRead(IR_01) == HIGH)
-  {
-    digitalWrite(LED_1_PIN, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED_1_PIN, LOW);
-  }
+  _setSpeed(70);
+  _motorsRun(FORWARD);
+  delay(10000);
+  _motorsRun(RELEASE);
+  delay(1000);
+
+  // if (digitalRead(IR_01) == HIGH)
+  // {
+  //   digitalWrite(LED_1_PIN, HIGH);
+  // }
+  // else
+  // {
+  //   digitalWrite(LED_1_PIN, LOW);
+  // }
 
   // blinkLed(1000);
+}
+
+void _setSpeed(int speed)
+{
+  motor1.setSpeed(speed);
+  motor2.setSpeed(speed);
+  motor3.setSpeed(speed);
+  motor4.setSpeed(speed);
+}
+
+void _motorsRun(int direction)
+{
+  motor1.run(direction);
+  motor2.run(direction);
+  motor3.run(direction);
+  motor4.run(direction);
 }
 
 void blinkLed(int time)
